025daee581b807e231eff4ac5e9cfaea1d774e8e
mppan <mppan@iu.edu>
2024-04-05 14:23:58 -0400

    Lab9 Submit

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 1;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://lab9-mattpan09.onrender.com";
+const host_remote = "https://c322-lab9-mattpan09.onrender.com";
 
 function getHost() {
     return (mode == 0) ? host_local : host_remote;
